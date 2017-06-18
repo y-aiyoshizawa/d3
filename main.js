221f1b3a14c57edb8812f8eb0ef6c478d6694661
yuta <aiyoshizawa.yuta@gmail.com>
2017-06-18 20:22:27 +0900

    bgm追加

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 window.onload = function(){
-
   var width = 800;
   var height = 400;
   var margin = 100;
@@ -10,9 +9,9 @@ window.onload = function(){
         "y":Math.random() * (height - (height / 10) * 2) + (height / 10),
         "page":[
           {
-            "str":"応用言語成果発表",
+            "str":"D3を使った４分間Cooking！",
             "size":40,
-            "x":300,
+            "x":100,
             "y":100
           },
           {
@@ -26,24 +25,6 @@ window.onload = function(){
             "size":20,
             "x":100,
             "y":300
-          }
-        ]
-      },
-      {
-        "x":Math.random() * (width - (width / 10) * 2) + (width / 10),
-        "y":Math.random() * (height - (height / 10) * 2) + (height / 10),
-        "page":[
-          {
-            "str":"テーマは",
-            "size":40,
-            "x":300,
-            "y":100
-          },
-          {
-            "str":"D3を使った３分間Cooking！",
-            "size":40,
-            "x":150,
-            "y":200
           }
         ]
       },
@@ -82,9 +63,21 @@ window.onload = function(){
         "y":Math.random() * (height - (height / 10) * 2) + (height / 10),
         "page":[
           {
-            "str":"この資料もD3！",
+            "str":"早速始めていきます！",
             "size":40,
             "x":200,
+            "y":200
+          }
+        ]
+      },
+      {
+        "x":Math.random() * (width - (width / 10) * 2) + (width / 10),
+        "y":Math.random() * (height - (height / 10) * 2) + (height / 10),
+        "page":[
+          {
+            "str":"この資料もD3だけで作っている！",
+            "size":40,
+            "x":100,
             "y":200
           }
         ]
@@ -138,10 +131,16 @@ window.onload = function(){
         okKey = true;
         break;
     }
-    if(okKey &&index === 3){
+    if(okKey &&index === 2){
       drowGraph();
     }else{
       deleteGraph()
+    }
+
+    if(index === 3){
+      bgmStart();
+    }else{
+      bgmStop();
     }
   });
 
@@ -191,7 +190,8 @@ window.onload = function(){
         style:"font-size:"+fontsize
       });
   }
-  var graphData = [100,400,300,200];
+
+  var graphData = [100,200,300,40];
   var graphHeight = 30;
   var margin = 10;
   var graph = d3.select("svg").append("g")
@@ -221,5 +221,28 @@ window.onload = function(){
           width:0
         })
     }
+  }
+
+  var bgmFlag = false;
+  var bgm = $("#bgm")[0];
+  function bgmStart(){
+    bgm.currentTime = 0;
+    bgm.volume = 1;
+    bgm.play();
+    bgmFlag = true;
+  }
+  function bgmStop(){
+    if(!bgmFlag)return;
+    setTimeout(function(){
+      var sub = bgm.volume / 10;
+      for(var i = 0; i < 10; i++){
+        setTimeout(function(){bgm.volume -= sub},100 * i);
+      }
+      setTimeout(function(){
+        bgm.volume = 0;
+        bgm.pause();
+      },1100);
+      bgmFlag = false;
+    },0);
   }
 }
